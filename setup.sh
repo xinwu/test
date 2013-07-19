201e0762207c4dc2568dc72f5e4ab25383f34fb3
Ubuntu <ubuntu@ip-10-164-74-165.ec2.internal>
2013-07-19 06:45:51 +0000

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#set up a brand new ruby on rails env
+
+path='/home/ubuntu/health'
+ip=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+port=80
+
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get install -y ruby1.9.1 ruby1.9.1-dev nodejs
+sudo gem install rails
+sudo gem install rdoc-data
+sudo rdoc-data --install
+sudo service apache2 stop
+sudo aptitude purge apache2
+mkdir $path
+cd $path
+rails new $path 
+rails server -b $ip -p $port
